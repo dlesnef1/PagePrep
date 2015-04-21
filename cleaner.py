2bdacf7ab03edf961b96b76c4ae9d670af98e9fc
mflaim1 <mflaim1@ithaca.edu>
2015-04-21 11:09:29 -0400

    cleaner update

--- cleaner.py
+++ cleaner.py
@@ -1,1 +1,17 @@
-__author__ = 'David'
+__author__ = 'Mariah and David'
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+class cleaner:
+        def removeCommon(self, text):
+            cachedStopWords = stopwords.words("english")
+            text = ' '.join([word for word in text.split() if word not in cachedStopWords])
+            return text
+        def tokenizeText(self,text):
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(text)  # return the tokens
+            return tokens
+
+
+test=cleaner()
+commonRemoved=test.removeCommon("Not long till the lunchtime beer session begins")
+print(test.tokenizeText(commonRemoved))
