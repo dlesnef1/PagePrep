9ed20a25c480a8630f9944cea7f0fc81696f8578
dlesnef1 <davidlesnefsky@gmail.com>
2015-05-01 12:31:43 -0400

    Naive Bayes with testing #2

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from methods import Methods
 
+
 class testing:
     def __init__(self):
         self.method = Methods()
         self.test()
 
     def test(self):
-        badWords = ['fuck','damn','work','cunt']
+        badWords = ['fuck','damn','work','cunt','bitch','whore']
 
         lines = self.openFile("testing.txt")
         results_NB = []
@@ -23,8 +24,18 @@ class testing:
             results_NB_noC_bad.append(self.method.rate_status(status,3,badWords))
 
         scores = [0,0,0,0]
+        scoresn =[0,0,0,0]
         for i in range(len(lines)):
-            rate = status = lines[i].split(";")[0]
+            rate = lines[i].split(";")[0]
+            if rate == 'negative':
+                if rate == results_NB[i]:
+                    scoresn[0] +=1
+                if rate == results_NB_noC[i]:
+                    scoresn[1] +=1
+                if rate == results_NB_bad[i]:
+                    scoresn[2] +=1
+                if rate == results_NB_noC_bad[i]:
+                    scoresn[3] +=1
             if rate == results_NB[i]:
                 scores[0] +=1
             if rate == results_NB_noC[i]:
@@ -33,7 +44,9 @@ class testing:
                 scores[2] +=1
             if rate == results_NB_noC_bad[i]:
                 scores[3] +=1
+
         print(scores)
+        print(scoresn)
 
     def openFile(self,name):
         this = open(name,'r')
@@ -41,5 +54,4 @@ class testing:
         for i in this.readlines():
             toReturn.append(i[:-1])
         return toReturn
-
 
